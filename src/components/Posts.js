c908097c85aac80339620737c6e5724106d925d5
Allen <allenmcndounger@gmail.com>
2020-04-10 10:59:28 +0300

    Iterating state object to the render method

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -13,9 +13,18 @@ class Posts extends Component {
         .then(data => this.setState({posts: data}))
     }
     render() {
+        // constructing a function that will iterate on each obj in post array ,then construct a JSX template
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.description}</p>
+            </div>
+        ))
         return (
             <div>
               <h1>Post</h1>  
+              {/* Call postItems function here */}
+              {postItems}
             </div>
         )
     }
