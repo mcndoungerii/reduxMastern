a30aedbf67ea9c2682aa7b9f7f1b2152f0f3011e
Allen <allenmcndounger@gmail.com>
2020-04-10 13:03:14 +0300

    configuring rootReducer to the store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
-import thunk from 'react-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 
