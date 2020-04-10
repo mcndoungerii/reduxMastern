6703797ad9cd438b5e74271ad281bd23873ade63
Allen <allenmcndounger@gmail.com>
2020-04-10 10:52:20 +0300

    component state

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
-    componentWillMount(){
-        console.log(12)
+    constructor(props){
+        super(props)
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => this.setState({posts: data}))
     }
     render() {
         return (
