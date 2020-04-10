500ecfc6912351db9584270bc1c7a7e3e2e13c81
Allen <allenmcndounger@gmail.com>
2020-04-10 17:53:22 +0300

    created a createPost action

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -8,3 +8,18 @@ export const fetchPosts = () => dispatch => {
             payload: posts
         }))
 }
+
+export const createPost = (postData) => dispatch => {
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(res => res.json())
+    .then(post => dispatch({
+        type: NEW_POST,
+        payload: post
+    }));
+}
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -20,15 +20,7 @@ class PostForm extends Component {
             title: this.state.title,
             body: this.state.body
         }
-        fetch('https://jsonplaceholder.typicode.com/posts',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(postData)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data));
+        // call action
     }
     render() {
         return (
