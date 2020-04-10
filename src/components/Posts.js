66122aacfae3f6f36a1408b253f0951c55ed055a
Allen <allenmcndounger@gmail.com>
2020-04-10 09:07:19 +0300

    Changing LifeCycle Hooks

---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Posts extends Component {
+    componentWillMount(){
+        console.log(12)
+    }
+    render() {
+        return (
+            <div>
+              <h1>Post</h1>  
+            </div>
+        )
+    }
+}
+
+export default Posts;
