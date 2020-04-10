f9f32e000d1297fd515d1cd2efaa51069d58c11b
Allen <allenmcndounger@gmail.com>
2020-04-10 11:16:51 +0300

    creating form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -9,6 +9,20 @@ class PostForm extends Component {
         return (
             <div>
                 <h1>Add Post</h1>
+                <form>
+                    <div>
+                        <label>Title</label>
+                        <br/>
+                        <input type="text" name="title"></input>
+                    </div>
+                    <div>
+                        <label>Body</label>
+                        <br/>
+                        <textarea name="body"></textarea>
+                    </div>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
